512123ebbccc41fe7e5a1e64fae7b205391fb583
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-07-20 18:48:16 +0600

    updated

--- app/user/dashboard/page.tsx
+++ app/user/dashboard/page.tsx
@@ -2,12 +2,14 @@ import Statistics from "@/components/home/Statistics";
 
 const page = () => {
   return (
-    <div className="space-y-2">
-      <h1 className="text-3xl font-bold tracking-tight">User Dashboard</h1>
-      <p className="text-muted-foreground">
-        Welcome back! Here&apos;s an overview of your activity.
-      </p>
-      <div className="container mx-auto mt-4">
+    <div>
+      <div className="space-y-2">
+        <h1 className="text-3xl font-bold tracking-tight">User Dashboard</h1>
+        <p className="text-muted-foreground">
+          Welcome back! Here&apos;s an overview of your activity.
+        </p>
+      </div>
+      <div className="container mx-auto">
         <Statistics />
       </div>
     </div>
